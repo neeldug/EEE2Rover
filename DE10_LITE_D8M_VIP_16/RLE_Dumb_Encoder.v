e35af5adb61f9089db15e87400cd7177612c44d7
George Karfakis <george.karfakis@gmail.com>
2021-05-31 00:57:50 +0300

    Dumb RLE filtering implementation
    RLE encode is still buggy and can't reliably handle >2 sequences. Fix in the works.

--- DE10_LITE_D8M_VIP_16/RLE_Dumb_Encoder.v
+++ DE10_LITE_D8M_VIP_16/RLE_Dumb_Encoder.v
@@ -1,35 +1,23 @@
 module RLE_Dumb_Encoder(
 input pixelin,
 input CLK,
-output reg [9:0]stream1,stream2,stream3,
+output reg [9:0]stream1,stream2,stream3,buffer,
 output reg im_end);
 
-// THIS RLE ENCODER IS SEVERELY CRIPPLED, AND ONLY CONSIDERS THE FIRST 3 STREAKS.
-// IT NEEDS SERIOUS OVERHAUL WITH (PROBABLY) AN EMBEDDED FIFO FOR PROPER USAGE.
+// THIS DUMB RLE ENCODER PERFORMS DUMB FILTERING BY ONLY KEEPING THE LARGEST SEQUENCES PER LINE.\
+// TODO: DELETE ALL SEQUENCES UNDER A CONST TO COMPLETE DUMB FILTERING.
 
 parameter IMAGE_W = 11'd25;
 
 reg prev = 0;
 reg[9:0] tally = 0;
 reg[10:0] indx = 0;
-reg[1:0] num = 0;
+reg[2:0] num = 0;
+
 
 always @ (posedge CLK)
 begin
-	case(num)
-		0:
-			stream1 <= tally;
-		2'd1:
-			stream2 <= tally;
-		2'd2:
-			stream3 <= tally;
-		default:
-			begin
-			stream1 <= 0;
-			stream2 <= 0;
-			stream3 <= 0;
-			end
-	endcase
+
 	if (indx != IMAGE_W) begin
 		if (indx==0) begin
 			stream1 <= 0;
@@ -53,6 +41,29 @@ begin
 		prev <= 0;
 		tally <= 0;
 	end
+	case(num) // WE ARE HANDLING REBASING USING NUM -- THIS ASSUMES OUR BLOCK DOESNT END IN BLACK. WONT BE A PROBLEM GENERALLY
+				// BUT MIGHT NEED TO BE CHECKED.
+		0:
+			stream1 <= tally; //WHITE
+		3'd1:
+			stream2 <= tally; //BLACK
+		3'd2:
+			stream3 <= tally; //WHITE
+		3'd3:
+			buffer <= tally; // fill buffer with 2nd black seq
+		3'd4: // handle 2nd black seq now.
+		begin
+			if (buffer > stream2) begin //rebase
+				stream1 <= indx-buffer-1;
+				stream2 <= buffer;
+				tally <= 2;
+			end else begin // ignore seq.
+				tally <= stream3+buffer+2;
+			end
+			num <= 2;
+			buffer <= 0;
+		end
+	endcase
 end
 
 
