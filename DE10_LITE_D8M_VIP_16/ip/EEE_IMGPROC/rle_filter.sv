49cda76b59560d33c14672617373d77ff5f4cdb0
nd419 <5161147+neeldug@users.noreply.github.com>
2021-06-09 13:00:46 +0100

    rle_filter

--- DE10_LITE_D8M_VIP_16/ip/EEE_IMGPROC/rle_filter.sv
+++ DE10_LITE_D8M_VIP_16/ip/EEE_IMGPROC/rle_filter.sv
@@ -2,7 +2,9 @@ module rle_filter(
     input logic clk,
     input logic rst,
     input logic valid_in,
-    input logic pixel_in
+    input logic pixel_in,
+    output logic [10:0] stream1, stream2, stream3,
+    output logic im_end
 );
 
     /*
@@ -28,8 +30,13 @@ module rle_filter(
         end
         else begin
             if (valid_in) begin
-                if (pixel_count == line_width-1) pixel_count <= 'b0;
-                else pixel_count <= pixel_count+11'b1;
+                if (pixel_count == line_width-1) begin
+                    pixel_count <= 'b0;
+                    im_end <= 1'b1;
+                end
+                else begin
+                    pixel_count <= pixel_count+11'b1;
+                end
             end
         end
     end
@@ -47,12 +54,17 @@ module rle_filter(
         end else begin
             if (valid_in) begin
                 if (pixel_count == 0) begin
+                    im_end <= 1'b1;
+                    stream1 <= largest_run_start + 1;
+                    stream2 <= largest_run;
+                    stream3 <= line_width - (largest_run_start + 1 + largest_run);
                     largest_run <= 'b0;
                     largest_run_start <= 'b0;
                     curr_run_start <= 'b0;
                     curr_run_length <= 'b0;
                 end
                 else begin
+                    im_end <= 1'b0;
                     if (pixel_in) begin
                         if (curr_run_length == 'b0) begin
                             // start new run
